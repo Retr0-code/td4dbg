4dddc19660dfe58fba119b7d7e1397a4fbdf9749
Retr0-code <nikk3695@gmail.com>
2025-09-20 13:43:21 +0300

    Reworked commands

--- include/IConsoleVariable.hpp
+++ include/IConsoleVariable.hpp
@@ -11,6 +11,10 @@ namespace td4 {
         virtual void SetValue(const std::string &value) = 0;
         
         virtual std::any GetValue(void) const = 0;
+        
+        virtual std::any operator*(void) const {
+            return this->GetValue();
+        }
     };
 
     using ConVarPtr = std::shared_ptr<IConsoleVariable>;
--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,25 @@
 #include <fstream>
 #include <iostream>
 
+#include <signal.h>
+
 #include "Emulator.hpp"
 #include "ConsoleInterface.hpp"
 #include "ConsoleVariables.hpp"
 
-static td4::ConVarInt::Type programStep{0};
+static td4::ConVarPtr programStep = std::make_shared<td4::ConVarInt>();
+
+void StopSimulation(int) {
+    programStep->SetValue(std::any{0});
+    std::cout << std::endl;
+}
+
+#define CastFloat(value) std::any_cast<td4::ConVarFloat::Type>(value)
+#define CastInt(value) std::any_cast<td4::ConVarInt::Type>(value)
 
 int main(int argc, const char **argv) {
+    signal(SIGINT, &StopSimulation);
+
     using namespace std::chrono;
 
     if (argc != 2) {
@@ -34,33 +46,31 @@ int main(int argc, const char **argv) {
     inputFile.read(reinterpret_cast<char*>(program.data()), fileSize);
 
     td4::ConsoleInterface cli;
-    cli.RegisterCommand("clock", std::make_shared<td4::ConVarFloat>())
-       .RegisterCommand("step",  std::make_shared<td4::ConVarInt>())
-       .RegisterCommand("run",  std::make_shared<td4::ConVarInt>())
-       .RegisterCommand("quit",  std::make_shared<td4::ConVarInt>());
+    td4::ConVarPtr frequency = std::make_shared<td4::ConVarFloat>();
+    td4::ConVarPtr run = std::make_shared<td4::ConVarInt>();
+    td4::ConVarPtr quit = std::make_shared<td4::ConVarInt>();
+    cli.RegisterCommand("clock", frequency)
+       .RegisterCommand("step",  programStep)
+       .RegisterCommand("run",   run)
+       .RegisterCommand("quit",  quit);
 
     td4::Emulator emulator(program);
-    td4::ConVarFloat::Type frequency;
-    td4::ConVarInt::Type quit{0};
+    std::cout << "Press ENTER to continue" << std::endl;
     while (true) {
-        if (programStep <= 0 || frequency == 0.f) {
+        if (CastInt(**programStep) <= 0 || CastFloat(**frequency) == 0.f) {
+
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             cli("run")->SetValue(std::any(0));
-            int run{std::any_cast<td4::ConVarInt::Type>(cli["run"])};
-            while (!run) {
+            while (!CastInt(**run)) {
                 std::cout << "> " << std::flush;
 
                 try {
                     cli.ReadCommand(std::cin);
-                    frequency = std::any_cast<td4::ConVarFloat::Type>(cli["clock"]);
-                    programStep = std::any_cast<td4::ConVarInt::Type>(cli["step"]);
-                    run = std::any_cast<td4::ConVarInt::Type>(cli["run"]);
-                    quit = std::any_cast<td4::ConVarInt::Type>(cli["quit"]);
                     
-                    if (quit)
+                    if (CastInt(**quit))
                         break;
 
-                    if (programStep && frequency == 0.f)
+                    if (CastInt(**programStep) && CastFloat(**frequency) == 0.f)
                         break;
 
                 } catch (...) {
@@ -69,15 +79,17 @@ int main(int argc, const char **argv) {
             }
         }
 
-        if (quit)
+        if (CastInt(**quit))
             break;
         
         steady_clock::time_point begin = steady_clock::now();
         steady_clock::time_point end = begin;
-        while (frequency != 0.f && duration_cast<milliseconds>(end - begin).count() < 1000.f / frequency)
+        while (CastFloat(**frequency) != 0.f &&
+            duration_cast<milliseconds>(end - begin).count() < 1000.f / CastFloat(**frequency))
+            
             end = steady_clock::now();
 
-        --programStep;
+        programStep->SetValue(CastInt(**programStep) - 1);
 
         td4::Registers status{emulator.Step()};
 
